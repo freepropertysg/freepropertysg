afc9b600f196add36d11e8b9fa76ba4ae7ea40a2
freepropertysg <freepropertysg@gmail.com>
2025-12-12 19:40:50 +0800

    proceed

--- generate-blog-index.js
+++ generate-blog-index.js
@@ -3,27 +3,23 @@ const fs = require("fs");
 const BLOG_DIR = "./blog";
 const INDEX_FILE = "./blog/index.html";
 
+const html = fs.readFileSync(INDEX_FILE, "utf8");
+
 const posts = fs.readdirSync(BLOG_DIR)
-  .filter(f => f.endsWith(".html") && f !== "index.html");
+  .filter(f => f.endsWith(".html") && f !== "index.html")
+  .sort()
+  .reverse();
 
 const list = posts.map(file => {
   const slug = file.replace(".html", "");
   const title = slug.replace(/-/g, " ")
     .replace(/\b\w/g, c => c.toUpperCase());
-  return `<li><a href="/blog/${slug}">${title}</a></li>`;
+  return `  <li><a href="/blog/${slug}">${title}</a></li>`;
 }).join("\n");
 
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Blog</title>
-</head>
-<body>
-<ul>
-${list}
-</ul>
-</body>
-</html>`;
+const updated = html.replace(
+  /<!-- BLOG-LIST-START -->[\s\S]*?<!-- BLOG-LIST-END -->/,
+  `<!-- BLOG-LIST-START -->\n<ul class="blog-list">\n${list}\n</ul>\n<!-- BLOG-LIST-END -->`
+);
 
-fs.writeFileSync(INDEX_FILE, html);
+fs.writeFileSync(INDEX_FILE, updated);
